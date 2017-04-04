6985c49bf3701b7213aec140d29e71d58f5987ca
John Kuhn <johnkuhn@dhcp-218-83.resnet.purdue.edu>
2017-04-04 00:10:43 -0400

    successfully compresses midi text, works for one file only

--- fileShrinker/compress.js
+++ fileShrinker/compress.js
@@ -2,14 +2,28 @@ function compress(){
 
 	var textArea = document.getElementById('toCompress');
 	var rawText = textArea.value.split('\n');
-	var blockCounter = 1;
+	var blockCounter = 0;
 
-	var compressedText = ["balls", "man", "", "balls"];
+	var compressedText = [];
 
 	for(var i = 0; i < rawText.length; i++){
-		console.log(rawText[i]);
+		if(i === 0 || (rawText[i].split(" ").indexOf("Text") === -1 && rawText[i].split(" ").indexOf("BA") !== -1)){
+			var line = "";
+			var count = 0;//want to get rid of the first three bits of text per line
+			for(var j = 0; j < rawText[i].split(" ").length; j++){
+				if(rawText[i].split(" ")[j] != ""){
+					if(i === 0 || count >= 3){
+						line += rawText[i].split(" ")[j] + " ";
+					}					
+					count++;
+				}
+			}
+			compressedText.push(line.trim());
+		}else if(rawText[i] == ""){
+			compressedText.push("");
+			blockCounter++;
+		}
 	}
-
 
 	textArea.value = compressedText.join("\n");
 
