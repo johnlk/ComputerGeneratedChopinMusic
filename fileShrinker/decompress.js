8038fff299773efa90819fc382ac119307efa599
John Landa Kuhn <jlkuhn@pod4-2.cs.purdue.edu>
2017-04-05 15:00:40 -0400

    decompress now takes short text and outputs midi readable text

--- fileShrinker/decompress.js
+++ fileShrinker/decompress.js
@@ -1,5 +1,24 @@
 function decompress(){
 
-	this.text = document.getElementById('toDecompress').value;
+	var textField = document.getElementById('toDecompress');
+	var expandedText = "";
+
+	var sectionCounter = 0;
+	var rawText = textField.value.split("\n");
+	
+	for(var i = 0; i < rawText.length; i++){
+		if(rawText[i].length == 0){
+			expandedText += "\n";
+			sectionCounter++;
+		}else if(rawText[i].indexOf("format") == -1){
+			expandedText += "BA " + sectionCounter + " CR " + rawText[i] + "\n";
+		}else{
+			expandedText += rawText[i] + "\n";
+		}
+	}
+
+	//console.log(textField.value);
+	
+	textField.value = expandedText;
 
 }
