1e6b560b5a0d91d36caf05d764540f7fdde97a3b
John Kuhn <johnkuhn@pal-nat186-27-77.itap.purdue.edu>
2017-04-06 17:15:05 -0400

    updated compress to remove 'end of track' tags

--- fileShrinker/compress.js
+++ fileShrinker/compress.js
@@ -7,7 +7,8 @@ function compress(){
 	var compressedText = [];
 
 	for(var i = 0; i < rawText.length; i++){
-		if(i === 0 || (rawText[i].split(" ").indexOf("Text") === -1 && rawText[i].split(" ").indexOf("BA") !== -1)){
+		if(i === 0 || (rawText[i].split(" ").indexOf("Text") === -1 && rawText[i].split(" ").indexOf("End") === -1
+					 && rawText[i].split(" ").indexOf("BA") !== -1)){
 			var line = "";
 			var count = 0;//want to get rid of the first three bits of text per line
 			for(var j = 0; j < rawText[i].split(" ").length; j++){
