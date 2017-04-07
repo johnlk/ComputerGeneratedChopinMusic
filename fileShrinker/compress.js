ac97584641be30bbeed38769d867c34e9a7237e3
John Kuhn <johnkuhn@dhcp-218-83.resnet.purdue.edu>
2017-04-06 23:40:30 -0400

    removed top line for format

--- fileShrinker/compress.js
+++ fileShrinker/compress.js
@@ -6,9 +6,10 @@ function compress(){
 
 	var compressedText = [];
 
-	for(var i = 0; i < rawText.length; i++){
-		if(i === 0 || (rawText[i].split(" ").indexOf("Text") === -1 && rawText[i].split(" ").indexOf("End") === -1
-					 && rawText[i].split(" ").indexOf("BA") !== -1)){
+	//i starts on 2 because we are skipping the formate lines
+	for(var i = 2; i < rawText.length; i++){
+		if(rawText[i].split(" ").indexOf("Text") === -1 && rawText[i].split(" ").indexOf("End") === -1
+					 && rawText[i].split(" ").indexOf("BA") !== -1){
 			var line = "";
 			var count = 0;//want to get rid of the first three bits of text per line
 			for(var j = 0; j < rawText[i].split(" ").length; j++){
